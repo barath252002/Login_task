2837cf70cd45aa6c59d4f1a91048c2d2ae7cee85
BARATH M <70840903+barath252002@users.noreply.github.com>
2023-03-17 22:02:39 +0530

    Update profile.js

--- GUVI GEEKS TASK 3/js/profile.js
+++ GUVI GEEKS TASK 3/js/profile.js
@@ -1,7 +1,16 @@
-    $.ajax({
-    url: "php/profile.php",
-    success: function(result){
-      if(result == 0){
-       document.getElementsByName("button1")[0].disabled = true;
-      }
-   }});
+  //   $.ajax({
+  //   url: "php/profile.php",
+  //   success: function(result){
+  //     if(result == 0){
+  //      document.getElementsByName("button1")[0].disabled = true;
+  //     }
+  //  }});
+
+   var byeEl = document.getElementById("bye");
+   function home()
+   {
+      byeEl.innerText = " See you again !!"
+      setTimeout(()=> {
+        window.location.href = 'login.html';			 }
+     ,3000);
+   }
